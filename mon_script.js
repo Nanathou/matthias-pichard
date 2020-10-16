d980e8354fb507653cd956ef4672c0e44a755f20
Nathalie <nathalie.poinot@capgemini.com>
2020-10-16 17:01:14 +0200

    suppression logs

--- mon_script.js
+++ mon_script.js
@@ -2,21 +2,18 @@
     var description = $( "#description" );
 
     $( "#btnClim" ).change(() => {
-        console.log("Hello 1");
         if ($( "#btnClim" ).prop("checked")) {
             description.css("transform", `translateX(0px)`);
         }
     });
 
     $( "#btnGainable" ).change(() => {
-        console.log("Hello 2");
         if ($( "#btnGainable" ).prop("checked")) {
             description.css("transform", `translateX(-33%)`);
         }
     });
 
     $( "#btnEntretien" ).change(() => {
-        console.log("Hello 3");
         if ($( "#btnEntretien" ).prop("checked")) {
             description.css("transform", `translateX(-66%)`);
         }
